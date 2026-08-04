93c88932988e98fd19a8693355b2388db08ca0e7
willlliammmsmithhh <67@felix-au.me>
2026-08-04 06:17:32 +0530

    Update cache.rs - Step 10

--- cache.rs
+++ cache.rs
@@ -1,3 +1,5 @@
 pub struct Cache { map: std::collections::HashMap<String, i32> }
 impl Cache {
     pub fn new() -> Self { Self { map: std::collections::HashMap::new() } }
+    pub fn set(&mut self, k: &str, v: i32) { self.map.insert(k.to_string(), v); }
+}
